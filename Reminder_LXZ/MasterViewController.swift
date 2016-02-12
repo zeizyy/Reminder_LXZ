e73b1a2a00990c60b4de509405337a8ff8549459
zeizyy <yz9fy@virginia.edu>
2016-02-12 11:38:37 -0500

    Comment

--- Reminder_LXZ/MasterViewController.swift
+++ Reminder_LXZ/MasterViewController.swift
@@ -11,6 +11,7 @@ import CoreData
 
 class MasterViewController: UITableViewController, NSFetchedResultsControllerDelegate {
 
+    // Comment
     var detailViewController: DetailViewController? = nil
     var managedObjectContext: NSManagedObjectContext? = nil
 
